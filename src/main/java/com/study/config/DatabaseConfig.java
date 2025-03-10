784dfd3b116256ba16582832da26c8f00dc46feb
pleadsori <plump74@gmail.com>
2025-03-11 00:08:59 +0900

    https://congsong.tistory.com/15 부분까지 진행. 4. Mapper 인터페이스 생성하기, 까지 하였음. 이해와 진행 필요.

---
+++ src/main/java/com/study/config/DatabaseConfig.java
@@ -0,0 +1,57 @@
+package com.study.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan(basePackages = "com.study.mapper") // MyBatis Mapper 인터페이스 위치 지정
+public class DatabaseConfig {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+
+    @Bean
+    public DataSource dataSource() {
+        return new PooledDataSource(
+                "org.mariadb.jdbc.Driver",
+                "jdbc:mariadb://localhost:3306/stock_community",
+                "root",
+                "soyoung1234" // 여기 네 DB 비밀번호로 변경!
+        );
+    }
+
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+//		factoryBean.setMapperLocations(context.getResources("classpath:/mappers/**/*Mapper.xml"));
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+
+}
